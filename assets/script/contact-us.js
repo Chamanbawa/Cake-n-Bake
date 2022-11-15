79d689fddd17149db78221218437056d2ac0793a
Gurveer <gurveer2202@gmail.com>
2022-11-14 23:06:03 -0600

    start js

--- assets/script/contact-us.js
+++ assets/script/contact-us.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function onEvent(event, selector, callback){
+    return selector.addEventListener(event, callback);
+}
+
+//Get HTML element by id
+function getElement(selector, parent = document){
+    return parent.getElementById(selector);
+}
+
+//Select HTML element by selector
+function select(selector, parent = document){
+    return parent.querySelector(selector);
+}
+
