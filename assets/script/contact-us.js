b52497535e41be972fcd603deaa2de4223e9b4fd
Gurveer <gurveer2202@gmail.com>
2022-11-14 23:21:45 -0600

    update js

--- assets/script/contact-us.js
+++ assets/script/contact-us.js
@@ -14,3 +14,79 @@ function select(selector, parent = document){
     return parent.querySelector(selector);
 }
 
+
+const form = select('form');
+const btn =select('.btn');
+
+const emailRegex = /^(?=^.{8,}$)[-_A-Za-z0-9]+([_.-][a-zA-Z0-9]+)*@[A-Za-z0-9]+([.-][a-zA-Z0-9]+)*\.[A-Za-z]{2,}$/;
+const postalRegex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+const nameRegex = /(^[a-zA-Z][a-zA-Z\s]{0,20}[a-zA-Z]$)/
+
+function validate(){
+    let firstName = select('.first').value.trim();
+    let lastName = select('.last').value.trim();
+    //let age = select('.age').value.trim();
+    let email = select('.email').value.trim();
+   // let postal = select('.postal').value.trim();
+  let text = select('.message').value.trim();
+    
+
+    function isValid(input){
+        if(Number.isInteger(input)){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    let message = '';
+    let valid = true;
+    let count = 0;
+
+    if(firstName.length ===0){
+        message+= 'First name is required\n';
+        valid = false;
+        count++;
+    }else if(!nameRegex.test(firstName)){
+        message+='A valid first name is required\n';
+        valid=false;
+    }
+
+    if(lastName.length ===0){
+        message+= 'Last name is required\n';
+        valid = false;
+        count++;
+    }else if(!nameRegex.test(lastName)){
+        message+='A valid last name is required\n';
+        valid=false;
+    }
+
+    if(text.length ===0){
+        message+= 'Message is required\n';
+        valid = false;
+        count++;
+    }
+
+    if(email.length===0){
+        message+='Email is required\n';
+        valid=false;
+        count++;
+    }else if(!emailRegex.test(email)){
+        message+='A valid email is required';
+        valid=false;
+    }
+
+if(count === 4){
+    alert('Fields with * are required');
+}else if (!valid){
+    alert(message);
+}else {
+    alert('Form submitted');
+    // form.submit();
+}
+}
+
+onEvent('click', btn, function(){
+    validate();
+});
